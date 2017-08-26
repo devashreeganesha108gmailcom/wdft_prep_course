c8cca29db464ef3be790b7c8c89dd8c0b41b70a4
Devashish Shrestha <devashishshrestha1@Devashishs-MBP.hitronhub.home>
2017-08-26 13:46:34 -0700

    push code

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,7 +2,7 @@
 Exercise #1
 Initialize an empty array below in the variable numbers.
 */
-var numbers;
+var numbers = [];
 console.log('Hello World');
 
 /*
@@ -10,8 +10,8 @@ Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<10;i++){
-    //use the array push() method here.
+for(var i=0;i<=10;i++){
+    numbers.push(i);
 }
 
 
